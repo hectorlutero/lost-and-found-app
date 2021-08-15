d443c1d39b324f0d9f67fa04f0e7088c6b58f3fa
Hector Siman <61216453+hectorlutero@users.noreply.github.com>
2021-08-15 20:51:43 -0300

    additional setups
    BrowserSync

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Achados e Perdidos</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Achados e Perdidos
                 </div>
 
                 <div class="links">
